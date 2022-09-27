7a308b8379f2e508e5f896694ef36cb4307bfe85
Rahul <77380705+RahulG1309@users.noreply.github.com>
2022-09-27 16:41:28 +0800

    Restored /data

--- data/data_anal.py
+++ data/data_anal.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import pandas as pd
-from math import log10
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("data.tsv", sep='\t')
-print(df.head())
-# print(df.describe())
-
-print(f"\nTotal Keys: {len(df['numVotes'])}")
-print(f"Unique Keys: {len(set(df['numVotes']))}\n")
-# Checking the number of unique keys (numVotes)
-
-data = df.groupby('numVotes').size()
-print(data.head())
-
-data = data.apply(lambda x: log10(x))
-print(data.head())
-
-# visualize and save
-ax = data.plot.hist()
-ax.set_xlabel("numVotes (log scaled)")
-ax.set_ylabel("frequency (log scaled)")
-plt.savefig("frequency.png")
-plt.show()
-=======
 import pandas as pd
 from math import log10
 import matplotlib.pyplot as plt
@@ -50,4 +24,3 @@ ax.set_xlabel("numVotes (log scaled)")
 ax.set_ylabel("frequency (log scaled)")
 plt.savefig("frequency.png")
 plt.show()
->>>>>>> origin/main
